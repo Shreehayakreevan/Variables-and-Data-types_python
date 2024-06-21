ff49547a18fc6dcd26ec24c20a7dc064d738af9b
Shreehayakreevan <spideyhayak@gmail.com>
2024-06-21 19:53:33 +0530

    initial commit

---
+++ Variables and Data Types (Day 2).py
@@ -0,0 +1,14 @@
+#This Python program demonstrates the use of different data types including strings, integers, floats, and booleans.
+#string
+Name = "Shreehayakreevan"
+#integer
+Age = 15
+#float
+Height = 162.8
+Weight = 45.8
+#boolean
+is_student = True
+print("Name of the person : ",Name)
+print("Age of the person : ",Age)
+print("Height and weight of the person : ",Height    ,Weight )
+print("He is a student :",is_student)
